45fe666c6ecb14ffe3a63b5c879ef45b6be8d984
Noritop <axel.mathieu74@free.fr>
2021-12-08 11:13:10 +0100

    clean

--- DATA CHALLENGE/custom_weighted_accuracy.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def weighted_accuracy(y_test, y_pred):
-    y_abs = np.abs(y_test)
-    norm = y_abs.sum()
-    score = ((np.sign(y_pred) == np.sign(y_test)) * y_abs).sum() / norm
-    return score
